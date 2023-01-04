b31ebc8a529f2d0e2649787c53df71e7abf309a5
junilab <help@junilab.co.kr>
2023-01-05 00:12:42 +0900

    add files

---
+++ flash_app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template, flash, jsonify, json, session, escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+	return render_template("main.html")
+
+#app.run(host='202.68.230.185')
